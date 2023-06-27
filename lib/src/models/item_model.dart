f24957259da9a7f5d8042d08bdedcb4d849481e2
steadysamwise4 <davenportsam44@gmail.com>
2023-06-27 18:10:20 -0500

    tests written and passing for api fetch methods

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ItemModel {
-  final int id;
-  final bool deleted;
-  final String type;
-  final String by;
-  final int time;
-  final String text;
-  final bool dead;
-  final int parent;
-  final List<dynamic> kids;
-  final String url;
-  final int score;
-  final String title;
-  final int descendants;
+  final int? id;
+  final bool? deleted;
+  final String? type;
+  final String? by;
+  final int? time;
+  final String? text;
+  final bool? dead;
+  final int? parent;
+  final List<dynamic>? kids;
+  final String? url;
+  final int? score;
+  final String? title;
+  final int? descendants;
 
   ItemModel.fromJson(Map<String, dynamic> parsedJson)
       : id = parsedJson['id'],
