65f517600d050118c51dd9e0086d708434e89fc9
steadysamwise4 <davenportsam44@gmail.com>
2023-07-04 12:16:31 -0500

    NewsDbProvider and Repository classes added logic to not repeat api calls

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 class ItemModel {
   final int? id;
@@ -29,4 +30,37 @@ class ItemModel {
         score = parsedJson['score'],
         title = parsedJson['title'],
         descendants = parsedJson['descendants'];
+
+  ItemModel.fromDb(Map<String, dynamic> parsedJson)
+      : id = parsedJson['id'],
+        deleted = parsedJson['deleted'] == 1,
+        type = parsedJson['type'],
+        by = parsedJson['by'],
+        time = parsedJson['time'],
+        text = parsedJson['text'],
+        dead = parsedJson['dead'] == 1,
+        parent = parsedJson['parent'],
+        kids = jsonDecode(parsedJson['kids']),
+        url = parsedJson['url'],
+        score = parsedJson['score'],
+        title = parsedJson['title'],
+        descendants = parsedJson['descendants'];
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      "id": id,
+      "type": type,
+      "by": by,
+      "time": time,
+      "text": text,
+      "parent": parent,
+      "url": url,
+      "score": score,
+      "title": title,
+      "descendants": descendants,
+      "dead": dead != null && dead == true ? 1 : 0,
+      "deleted": deleted != null && deleted == true ? 1 : 0,
+      "kids": jsonEncode(kids)
+    };
+  }
 }
--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' show Client;
-
+import 'dart:async';
 import '../models/item_model.dart';
 
 const _root = 'https://hacker-news.firebaseio.com/v0';
@@ -8,14 +8,14 @@ const _root = 'https://hacker-news.firebaseio.com/v0';
 class NewsApiProvider {
   Client client = Client();
 
-  fetchTopIds() async {
+  Future<List<int>> fetchTopIds() async {
     final response = await client.get(Uri.parse('$_root/topstories.json'));
     final ids = json.decode(response.body);
 
-    return ids;
+    return ids.cast<int>();
   }
 
-  fetchItem(int id) async {
+  Future<ItemModel> fetchItem(int id) async {
     final response = await client.get(Uri.parse('$_root/item/$id.json'));
     final parsedJson = json.decode(response.body);
 
