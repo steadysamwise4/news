d40710d2ed54c17dc4fd17d011a6f876e17fb4b0
steadysamwise4 <davenportsam44@gmail.com>
2023-07-25 11:30:02 -0500

    itemId now passed into improved News Detail widget through the route, and displayed

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
 class NewsDetail extends StatelessWidget {
+  final int itemId;
+
+  const NewsDetail({super.key, required this.itemId});
+
+  @override
   Widget build(context) {
-    return const Text('Im a news detail');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail'),
+      ),
+      body: Text('$itemId'),
+    );
   }
 }
