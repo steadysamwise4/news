ff664f41fd187129f031a35cb7893bb6e5252610
steadysamwise4 <davenportsam44@gmail.com>
2023-07-28 10:45:27 -0500

    recursive child comments wired up, and all comments styled

--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -5,8 +5,13 @@ import '../models/item_model.dart';
 class Comment extends StatelessWidget {
   final int itemId;
   final Map<int, Future<ItemModel?>> itemMap;
+  final int depth;
 
-  const Comment({super.key, required this.itemId, required this.itemMap});
+  const Comment(
+      {super.key,
+      required this.itemId,
+      required this.itemMap,
+      required this.depth});
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +22,41 @@ class Comment extends StatelessWidget {
           return const Text('Still loading comment');
         }
 
-        return Text(snapshot.data!.text ?? '');
+        final item = snapshot.data;
+
+        final children = <Widget>[
+          ListTile(
+            title: buildText(item),
+            subtitle:
+                item!.by == "" ? const Text("Deleted") : Text(item.by ?? ''),
+            contentPadding: EdgeInsets.only(right: 16.0, left: depth * 16.0),
+          ),
+          const Divider()
+        ];
+
+        for (var kidId in snapshot.data!.kids!) {
+          children.add(
+            Comment(
+              itemId: kidId,
+              itemMap: itemMap,
+              depth: depth + 1,
+            ),
+          );
+        }
+
+        return Column(
+          children: children,
+        );
       }),
     );
+  }
+
+  Widget buildText(ItemModel? item) {
+    final text = item!.text!
+        .replaceAll('&#x27;', "'")
+        .replaceAll('<p>', '\n\n')
+        .replaceAll('</p>', '');
+
+    return Text(text);
   }
 }
